021180720a38566cfac1ad6cf9dc78266b6daffd
ShreeeBhat06 <shreelaxmibhat9685@gmail.com>
2026-07-09 17:10:18 +0530

    Spring webapp

---
+++ src/main/java/org/jsp/cafeapp/HomeController.java
@@ -0,0 +1,13 @@
+package org.jsp.cafeapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "cafe";
+    }
+}
